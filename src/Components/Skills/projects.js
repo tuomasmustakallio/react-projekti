10212f7b40b7cf8ac4835cb5c6e2d4fdf38737f6
Elmurmeli <79470113+Elmurmeli@users.noreply.github.com>
2022-01-09 16:05:57 +0200

    Projects.js file created

---
+++ src/Components/Skills/projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Projects(props){
+
+    const projectName = "Mobiilipeli"
+
+    return (
+    <div className="project">
+        <h3>{projectName}</h3>
+    </div>
+    );
+}
