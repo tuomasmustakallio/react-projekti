04474f38deb5c72b1dd830819dcfa762185abdb8
Jalex28 <joonaaleks@gmail.com>
2022-01-09 16:36:41 +0200

    Added file selection for profile picture

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'react-traning-sample-api/styles.css';
 import { getPosts } from 'react-traning-sample-api';
 import './App.css';
 import { Skills } from './Components/Skills/skills';
-import {Image} from './Components/Info/image';
+import Image from './Components/Info/image';
 
 function App() {
 
@@ -18,12 +18,12 @@ function App() {
   });
 
   if (loading) {
-    return <div className="posts-app">Ladataan</div>
+    return <div className="posts-app">Loading</div>
   }
 
   return <>
   <div class="right"><Skills/></div>
-  <Image/>
+  <div class="upleft"><Image/></div>
   </>
 }
 
