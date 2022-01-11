f44c640ff047afcdfd2c576ec290f1678d19ab64
Elmurmeli <79470113+Elmurmeli@users.noreply.github.com>
2022-01-11 15:18:51 +0200

    Modal for projects

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Project } from "./project";
 import './projects.css';
+import {useState} from 'react'
+import { Modal } from '../Skills/Modal.js';
 
 
 export function Projects(props){
 
     const testiprojekti = "Mobiilipeli";
     const testiInfo = "FGJ 2021";
+    const [showModal, setShowModal] = useState(false);
+
+    function hideModal(){
+        setShowModal(false);
+    }
 
     function clickButton(){
         alert('Yahhuu!!!');
@@ -21,10 +28,16 @@ export function Projects(props){
             projectName={testiprojekti}
             projectInfo={testiInfo}/>
         </div>
-        <div class="Button">
-            <button onClick={clickButton}>
-                Add project
-            </button>
+        <div class="Skills">
+            <Modal show={showModal} handleClose={hideModal}>
+                <h3 class="Skill">Project:</h3>
+                <div class="Skill" ><input type="text"/></div>
+                <h3 class="Skill">What was the project like?</h3>
+                <div class="Skill" ><input type="text"/></div>
+                <div class="Skill"><button >Add project</button></div>
+                
+            </Modal>
+            <button class="Button" onClick={() => {setShowModal(true)}}>Add  project</button>
         </div>
         </>
     </div>
