762c6fda1965678f403116fea0fc65cb12325396
tuomasmustakallio <tuoppi38@gmail.com>
2022-01-12 19:32:55 +0200

    adding languages is alot prettier now

--- src/Components/Skills/Modal.js
+++ src/Components/Skills/Modal.js
@@ -1,16 +1,19 @@
-import './Modal.css';
-import React from 'react';
+import "./style.css";
+import React from "react";
+import Button from '@mui/material/Button';
 
-export function Modal ({ handleClose, show, children }){
+export function Modal({ handleClose, show, children }) {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
 
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
         {children}
-        <div class="button"><button onClick={handleClose}>
-          Close
-        </button></div>
+        <div class="button" >
+          <Button variant="contained"onClick={handleClose}>
+            Close
+          </Button>
+        </div>
       </section>
     </div>
   );
--- src/Components/Skills/skill.js
+++ src/Components/Skills/skill.js
@@ -1,15 +1,14 @@
 import React from "react";
-import ProgressBar from 'react-bootstrap/ProgressBar'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export function Skill(props){
+export function Skill(props) {
+  const { skillName, skillAmount } = props;
 
-    const { skillName, skillAmount } = props;
-
-    return (
-    <div className="skill">
-        <h3>{skillName}</h3>
-        <ProgressBar now={skillAmount} />
+  return (
+    <div className="Skill">
+      <h3>{skillName}</h3>
+      <ProgressBar now={skillAmount} />
     </div>
-    );
+  );
 }
