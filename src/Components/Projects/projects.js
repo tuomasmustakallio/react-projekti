a96de72320dcd327928d6e9823cb605cbc31f24c
Elmurmeli <79470113+Elmurmeli@users.noreply.github.com>
2022-01-12 19:32:24 +0200

    Remove and add buttons were added to the project category

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -15,12 +15,23 @@ export function Projects(props){
         {projectName: "YAHHUU", projectInfo: "WOHHUU"}
     ]);
 
-    const handleChange = e => {
+    const handleChange = (e, index) => {
         const { name, value } = e.target;
-        setInputList({
-            ...inputList,
-            [name]:value
-        });
+        
+        const list = [...inputList];
+        list[index][name] = value;
+
+        setInputList(list);
+    }
+
+    const handleAddInput = () => {
+        setInputList([...inputList, {projectName: "", projectInfo: ""}]);
+    }
+
+    const handleRemoveInput = index => {
+        const list = [...inputList];
+        list.splice(index, 1);
+        setInputList(list)
     }
 
     const handleMouseEnter = e => {
@@ -37,45 +48,57 @@ export function Projects(props){
 
     return (
         <div className="projects">
-            <>
             <div class="Header"><h1>Projects</h1></div>
             <div class="Projects">
                 <Project
                 projectName={testiprojekti}
                 projectInfo={testiInfo}/>
             </div>
-            <div class="Project">
-                <Modal show={showModal} handleClose={hideModal}>
-                    <h3 class="Project">Project:</h3>
-                    <div class="Project" >
-                        <input
-                            type="text"
-                            name= "projectName"
-                            placeholder= "Project Name"
-                            className="project"
-                            value={inputList.projectName}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <h3 class="Project">Description</h3>
-                    <div class="Project" >
-                        <input
-                            type="text"
-                            name= "projectInfo"
-                            placeholder= "Description"
-                            value={inputList.projectInfo}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div class="Project"><button >Add project</button></div>
-                </Modal>
-                <button  onMouseEnter={handleMouseEnter}onMouseLeave={handleMouseLeave} className="Button" onClick={() => {setShowModal(true) }} >Add  project</button >
-            </div>
+            {inputList.map((item,i) => {
+                return(
+                    <>
+                    <div class="Project">
+                        <Modal show={showModal} handleClose={hideModal}>
+                            <h3 class="Project">Project:</h3>
+                            <div key={i}  class="Project" >
+                                <input
+                                    type="text"
+                                    name= "projectName"
+                                    placeholder= "Project Name"
+                                    className="project"
+                                    value={item.projectName}
+                                    onChange={e => handleChange(e, i)}
+                                />
+                            </div>
+                            <h3 class="Project">Description</h3>
+                            <div class="Project" >
+                                <input
+                                    type="text"
+                                    name= "projectInfo"
+                                    placeholder= "Description"
+                                    value={item.projectInfo}
+                                    onChange={e => handleChange(e, i)}
+                                />
+                            </div>
+                            <div class="Project"><button
+                                value="Add"
+                                onClick={handleAddInput}
+                            >Add project</button></div>
+                        </Modal>
+                        <button  onMouseEnter={handleMouseEnter}onMouseLeave={handleMouseLeave} className="Button"
+                            onClick={() => handleRemoveInput(i)}
+                        >Delete project
+                        </button >
+                        {inputList.length - 1 === i && <button  onMouseEnter={handleMouseEnter}onMouseLeave={handleMouseLeave} className="Button" onClick={() => {setShowModal(true) }} 
 
-            <pre>
+                        >Add  project</button >}
+                    </div>       
+                    </>             
+                    );
+                })}
+                <pre>
                 {JSON.stringify(inputList, null, 2)}
-            </pre>
-            </>
+                </pre>
         </div>
 
     );
