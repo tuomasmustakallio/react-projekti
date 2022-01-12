55ce444bb6e23ea9c2f9bdb282a4c32f4db3ca06
Elmurmeli <79470113+Elmurmeli@users.noreply.github.com>
2022-01-12 17:47:49 +0200

    Input field content show now on the main page

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -10,6 +10,18 @@ export function Projects(props){
     const testiprojekti = "Mobiilipeli";
     const testiInfo = "FGJ 2021";
     const [showModal, setShowModal] = useState(false);
+    
+    const [inputList, setInputList] = useState([
+        {projectName: "YAHHUU", projectInfo: "WOHHUU"}
+    ]);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setInputList({
+            ...inputList,
+            [name]:value
+        });
+    }
 
     const handleMouseEnter = e => {
         e.target.style.background = "grey"
@@ -22,35 +34,49 @@ export function Projects(props){
         setShowModal(false);
     }
 
-    function clickButton(){
-        alert('Yahhuu!!!');
-    }
 
     return (
-    <div className="projects">
-        <>
-        <div class="Header"><h1>Projects</h1></div>
-        <div class="Projects">
-            <Project
-            projectName={testiprojekti}
-            projectInfo={testiInfo}/>
+        <div className="projects">
+            <>
+            <div class="Header"><h1>Projects</h1></div>
+            <div class="Projects">
+                <Project
+                projectName={testiprojekti}
+                projectInfo={testiInfo}/>
+            </div>
+            <div class="Project">
+                <Modal show={showModal} handleClose={hideModal}>
+                    <h3 class="Project">Project:</h3>
+                    <div class="Project" >
+                        <input
+                            type="text"
+                            name= "projectName"
+                            placeholder= "Project Name"
+                            className="project"
+                            value={inputList.projectName}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <h3 class="Project">Description</h3>
+                    <div class="Project" >
+                        <input
+                            type="text"
+                            name= "projectInfo"
+                            placeholder= "Description"
+                            value={inputList.projectInfo}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div class="Project"><button >Add project</button></div>
+                </Modal>
+                <button  onMouseEnter={handleMouseEnter}onMouseLeave={handleMouseLeave} className="Button" onClick={() => {setShowModal(true) }} >Add  project</button >
+            </div>
+
+            <pre>
+                {JSON.stringify(inputList, null, 2)}
+            </pre>
+            </>
         </div>
-        <div class="Project">
-            <Modal show={showModal} handleClose={hideModal}>
-                <h3 class="Project">Project:</h3>
-                <div class="Project" ><input type="text"/></div>
-                <h3 class="Project">Description</h3>
-                <div class="Project" ><input type="text"/></div>
-                <div class="Project"><button >Add project</button></div>
-            </Modal>
-            <button  onMouseEnter={handleMouseEnter}onMouseLeave={handleMouseLeave} className="Button" onClick={() => {setShowModal(true) }}
-                
-            >
-                Add  project
-            </button >
-        </div>
-        </>
-    </div>
 
     );
 
